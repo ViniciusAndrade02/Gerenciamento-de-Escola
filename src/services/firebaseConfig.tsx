24446347add6e5be4e78998d11d30dfdfc960d76
baldancam <baldancama@gmail.com>
2024-11-25 14:01:30 -0300

    add chat

--- src/services/firebaseConfig.tsx
+++ src/services/firebaseConfig.tsx
@@ -1,19 +1,15 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAvp1eqvH4vHWBL77yzzoas8SKqa68Nzw",
-  authDomain: "chat-escola-extensao.firebaseapp.com",
-  databaseURL: "https://chat-escola-extensao-default-rtdb.firebaseio.com",
-  projectId: "chat-escola-extensao",
-  storageBucket: "chat-escola-extensao.firebasestorage.app",
-  messagingSenderId: "352697063588",
-  appId: "1:352697063588:web:05249738b8d40e93feee31"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
-
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const databaseApp = getFirestore(app);
